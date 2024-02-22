e45a244985c0c9d3b73651296fd7e34061e2940e
Micha Yanis <yanismicha02@gmail.com>
2024-02-22 10:10:41 +0100

    add documenting data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' World Health Organization TB data
+#'
+#' Data of waste from medicines site information.
+#'
+#'
+#' @format ## `Tonnage`
+#' A data frame with 188 rows and 6 columns:
+#' \describe{
+#'   \item{Region}{Region name}
+#'   \item{Maison.Mere}{Maison.mere name}
+#'   \item{Site}{Site name}
+#'   \item{Tonnages.DIM}{Number of DIM in dumpster (in tons)}
+#'   \item{Nbre.de.rotation}{Number of times dumpster is emptied}
+#'   \item{Compacteur}{1 if the site has a compactor instead of a dumpster, 0 otherwise}
+#' }
+#' @source <https://www.cyclamed.org/>
+"Tonnage"
