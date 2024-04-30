484e0b86fb6de3bb8e13466ea6a40c6ab0e7b198
Tony Dassin M <90778349+saulktd@users.noreply.github.com>
2024-04-30 07:45:26 +0200

    update tab_data

--- R/mod_TabData.R
+++ R/mod_TabData.R
@@ -8,41 +8,45 @@
 #'
 #' @importFrom shiny NS tagList
 mod_TabData_ui <- function(id) {
+  # définition de l'interface utilisateur pour afficher et interagir avec les données dans un tableau.
   ns <- NS(id)
   tabItem(tabName = "data",
           fluidPage(
             tags$head(
               tags$style(HTML("
-    /* CSS pour améliorer le tableau DT */
-    .dataTable {
-      border-collapse: collapse;
-    }
-    table.dataTable thead th, table.dataTable tbody td {
-      border: 1px solid #ddd;
-    }
-    table.dataTable thead th {
-      background-color: #f9f9f9;
-    }
-    /* Ajout de bordure et ombre pour la rangée éditable */
-    table.dataTable tr.shiny-input-container {
-      border: 2px solid #ccc;
-      box-shadow: 0 2px 3px #ccc;
-    }
-    /* Style pour les boutons */
-    .btn {
-      margin-right: 10px;
-    }
-    /* Styles personnalisés pour les notifications */
-    .shiny-notification {
-      background-color: #cccccc !important; /* Couleur de fond gris */
-      color: black; /* Couleur de texte */
-      border-left-color: #999999 !important; /* Couleur de la bordure à gauche */
-    }
-  "))
+
+                .dataTable {
+                  border-collapse: collapse;
+                }
+                table.dataTable thead th, table.dataTable tbody td {
+                  border: 1px solid #ddd;
+                }
+                table.dataTable thead th {
+                  background-color: #f9f9f9;
+                }
+
+                table.dataTable tr.shiny-input-container {
+                  border: 2px solid #ccc;
+                  box-shadow: 0 2px 3px #ccc;
+                }
+
+                .btn {
+                  margin-right: 10px;
+                }
+
+                .shiny-notification {
+                  background-color: #cccccc !important;
+                  color: black;
+                  border-left-color: #999999 !important;
+                }
+              "))
             ),
+
+            # Première rangée de l'interface utilisateur avec une marge en haut pour l'espacement.
             fluidRow(
               column(width = 1,
                      div(style = "margin-top: 15rem;",
+                         # Dropdown pour ajouter de nouvelles données avec différents champs de saisie.
                          dropdown(inputId = ns("modifyDataButton"),
                                   animate = TRUE, style = "pill", color = "success",
                                   tags$h4("Veuillez remplir chaque champ avant de rajouter une nouvelle ligne :"),
@@ -59,17 +63,25 @@ mod_TabData_ui <- function(id) {
                          )
                      )
               ),
-              column(width = 7,
-                     h1("Tableaux des grossistes répartiteurs"),
-                     DTOutput(ns('table_data'), width = "100%"),
-                     actionButton(ns("edit_btn"), "Edit Selected Row", icon = icon("edit"), class = "btn-success"),
-                     actionButton(ns("deleteBtn"), "Delete Selected Rows", icon = icon("trash"), class = "btn-danger")
+              column(
+                width = 7,
+                # Titre principal du tableau.
+                h1("Tableaux des sites de grossistes répartiteurs"),
+                # Sortie UI pour les boutons dynamiques (éditer, supprimer,).
+                uiOutput(ns('dynamic_buttons')),
+                # Sortie du tableau de données interactif.
+                DTOutput(ns('table_data'), width = "100%")
               )
             ),
+            # Deuxième rangée de l'interface utilisateur pour l'import de fichiers et la sauvegarde des données.
             fluidRow(
-              column(width = 9,
+              column(
+                width = 6,
+                fileInput(ns("fileInput"), "Nouvelles données", accept = ".csv", buttonLabel = "Parcourir...", placeholder = "Aucun fichier sélectionné")
+              ),
+              column(width = 4,
                      div(style = "text-align: right;",
-                         downloadBttn(outputId = ns("save_data"), label = "Sauvegarder en CSV", color = "success", size = "md", style = "gradient")
+                         downloadBttn(outputId = ns("save_data"), label = "Sauvegarder", color = "success", size = "md", style = "gradient")
                      )
               )
             )
@@ -84,30 +96,40 @@ mod_TabData_ui <- function(id) {
 mod_TabData_server <- function(id,r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    ####### esthetique de la data #########
+    # Génère dynamiquement le tableau de données pour l'interface utilisateur.
+    # 'r$data' est un objet reactiveValues qui contient les données à afficher.
     output$table_data <- DT::renderDataTable({
       DT::datatable(
         data = r$data,
-        editable = TRUE,
-        selection = 'multiple',
+        editable = TRUE,                # Permet l'édition des cellules du tableau.
+        selection = 'multiple',         # Permet de sélectionner plusieurs lignes pour les opérations en lot.
         options = list(
+          autoWidth = FALSE,
+          language = list(
+            info = "Site _START_ à _END_ sur un total de _TOTAL_ Sites",
+            lengthMenu = "Afficher _MENU_ sites"
+          ),
           pageLength = 10,
           lengthMenu = c(10, 50, 100),
-          autoWidth = TRUE,
           searchHighlight = TRUE,
           columnDefs = list(
-            list(targets = c(5), # Indice de la colonne 'Compacteur'
-                 render = JS(
-                   "function(data, type, row) {",
-                   "  return data == 0 ? '\u274c false' : '\u2714\ufe0f true';",
-                   "}"
-                 )),
-            list(targets = "_all",
-                 className = "dt-center")
+            list(
+              targets = c(5),
+              render = JS(
+                "function(data, type, row) {",
+                "  return data == 0 ? '\u274c false' : '\u2714\ufe0f true';",
+                "}"
+              )
+            ),
+            list(
+              targets = "_all",
+              className = "dt-center"
+            )
           )
         ),
         filter = 'top',
-        rownames = FALSE
+        rownames = FALSE,
+        colnames = c("Region", "Maison mere", "Site", "Tonnage de DIM", "Nombre de rotations", "Compacteur")
       ) %>%
         formatStyle(
           columns = c('Tonnages.DIM'),
@@ -117,14 +139,94 @@ mod_TabData_server <- function(id,r){
         )
     })
 
+    # boutons dynamic en fonction des lignes sélectionnées.
+    output$dynamic_buttons <- renderUI({
+      selected_rows <- input$table_data_rows_selected
 
-    ######## ajouter une ligne dans la data #########
+      button_list <- tagList()
+
+      # Ajoute un bouton 'Supprimer' si au moins une ligne est sélectionnée.
+      if (length(selected_rows) >= 1) {
+        button_list <- tagAppendChild(button_list, actionButton(ns("deleteBtn"), "Supprimer", icon = icon("trash"), class = "btn-danger"))
+      }
+
+      # Ajoute un bouton 'Éditer' si exactement une ligne est sélectionnée.
+      if (length(selected_rows) == 1) {
+        button_list <- tagAppendChild(button_list, actionButton(ns("edit_btn"), "Éditer", icon = icon("edit"), class = "btn-success"))
+      }
+
+      return(button_list)
+    })
+
+
+    # observeEvent lorsque l'utilisateur télécharge un fichier via input$fileInput.
+    observeEvent(input$fileInput, {
+      # Assure qu'un fichier a bien été téléchargé avant de poursuivre.
+      req(input$fileInput)
+
+      tryCatch({
+
+        newData <- read.csv(input$fileInput$datapath, stringsAsFactors = FALSE)
+        lowerCaseNames <- tolower(names(newData))
+
+        # Définition d'une liste de mots-clés pour identifier des colonnes attendues.
+        keywords <- list(
+          region = "region|regions",
+          maison.mere = "maison|mere|maison.mere",
+          site = "site|sites",
+          tonnages.dim = "dim|tonnages|tonnes|tonnages.dim",
+          nbre.de.rotation = "rotation|rotations|nbre.de.rotation",
+          compacteur = "compacteur|compacteurs|benne|bennes"
+        )
+
+        # Vérification de la présence des mots-clés dans les noms de colonnes pour s'assurer que
+        # toutes les colonnes nécessaires sont présentes.
+        match_columns <- function(data_names, keywords) {
+          sapply(keywords, function(keyword) {
+            any(grepl(keyword, data_names))
+          })
+        }
+
+        matches <- match_columns(lowerCaseNames, keywords)
+        # Si toutes les colonnes requises ne sont pas présentes, informer l'utilisateur.
+        if (!all(matches)) {
+          missing_cols <- names(matches)[!matches]
+          showNotification(
+            paste("Le fichier importé manque des colonnes essentielles ou contient des erreurs de nommage:", paste(missing_cols, collapse=", ")),
+            type = "error",
+            duration = 10
+          )
+          # Arret du traitement supplémentaire et conservation les données existantes.
+          return()
+        }
+
+        # Si toutes les colonnes requises sont présentes, mettre à jour les données dans l'application.
+        names(newData) <- c("Region", "Maison.Mere", "Site", "Tonnages.DIM", "Nbre.de.rotation", "Compacteur")
+        r$data <- newData
+        # Sauvegarder les nouvelles données dans un fichier CSV local pour une utilisation ultérieure.
+        write.csv(r$data, "Tonnage.csv", row.names = FALSE)
+        # Mettre à jour les données dans l'environnement global pour qu'elles soient disponibles partout dans l'application.
+        Tonnage <- r$data
+        usethis::use_data(Tonnage, overwrite = TRUE)
+
+        # En cas d'erreur lors de la lecture du fichier, affichage d'une notification à l'utilisateur.
+      }, error = function(e) {
+        showNotification("Une erreur est survenue en lisant le fichier: ", e$message, type = "error", duration = 6)
+      }, warning = function(w) {
+        showNotification("Message d'attention: ", w$message, type = "warning")
+      })
+    })
+
+    ########  bouton pour ajouter une nouvelle ligne de données.
     observeEvent(input$newRow, {
       error_message <- NULL
 
+      # Vérifie que la valeur entrée pour les tonnages de DIM est positive.
       if (input$c4 < 0) {
         error_message <- "La valeur du tonnages de DIM doit être positive."
       }
+
+      # Vérifie que le nombre de rotations entré est également positif.
       if (input$c5 < 0) {
         error_message <- paste0(error_message, if (!is.null(error_message)) " " else "", "La valeur du nombre de rotations doit être positive.")
       }
@@ -132,64 +234,99 @@ mod_TabData_server <- function(id,r){
       if (!is.null(error_message)) {
         showNotification(error_message, type = "error")
       } else if (isTruthy(input$c1) && isTruthy(input$c2) && isTruthy(input$c3)) {
+        # Si toutes les entrées nécessaires sont valides, demande une confirmation avant d'ajouter la ligne.
         confirmSweetAlert(
-          session = session, inputId = "myconfirmation", type = "info",
+          session = session,
+          inputId = "myconfirmation",
+          type = "info",
           title = "Êtes-vous sûr de vouloir rajouter cette nouvelle ligne ?",
-          danger_mode = TRUE, btn_labels = c("NON", "OUI")
+          danger_mode = TRUE,
+          btn_labels = c("NON", "OUI")
         )
       } else {
-        showNotification("Veuillez remplir tous les champs correctement avant d'ajouter une nouvelle ligne.", type = "error")
+        # Si certains champs ne sont pas remplis correctement, informe l'utilisateur.
+        showNotification("Veuillez remplir tous les champs correctement avant d'ajouter une nouvelle ligne.", type = "error", duration= 5)
       }
     })
 
 
 
-
+    # confirmation pour ajouter une nouvelle ligne de données.
     observeEvent(input$myconfirmation, {
       if (isTRUE(input$myconfirmation)) {
+        # Récupère les données actuelles stockées dans l'objet réactif `r$data`.
         currentData <- r$data
-        newRow <- data.frame(Region=input$c1,Maison.Mere=input$c2,Site=input$c3,Tonnages.DIM=input$c4,Nbre.de.rotation=input$c5,Compacteur=ifelse(input$c6=="Compacteur",1,0))
-        currentData <- rbind(newRow,currentData)
+
+        # Nouvelle ligne de données à partir des entrées de l'utilisateur.
+        newRow <- data.frame(
+          Region = input$c1,
+          Maison.Mere = input$c2,
+          Site = input$c3,
+          Tonnages.DIM = input$c4,
+          Nbre.de.rotation = input$c5,
+          Compacteur = ifelse(input$c6 == "Compacteur", 1, 0)
+        )
+        currentData <- rbind(newRow, currentData)
+
         r$data <- currentData
       }
-      saveRDS(r$data,"Tonnage.rda")
-      Tonnage <- readRDS("Tonnage.rda")
+
+      # Sauvegarde le dataframe mis à jour dans un fichier CSV nommé 'Tonnage.csv'.
+      write.csv(r$data, "Tonnage.csv", row.names = FALSE)
+      # Mise à jour des données dans l'environnement global pour qu'elles soient disponibles partout dans l'application.
+      Tonnage <- read.csv("Tonnage.csv")
       usethis::use_data(Tonnage, overwrite = TRUE)
     })
 
 
+    # bouton de suppression.
     observeEvent(input$deleteBtn, {
-      selectedRows <- input$table_data_rows_selected
-      if (length(selectedRows) > 0) {
-        showModal(modalDialog(
-          title = "Confirmation de la suppression",
-          "Êtes-vous sûr de vouloir supprimer la sélèction ?",
-          footer = tagList(
-            modalButton("Annuler"),
-            actionButton(ns("confirmDelete"), "Confirmer", class = "btn-primary")
-          )
-        ))
-      }
-
-
-    })
-
-    observeEvent(input$confirmDelete, {
-      removeModal()
+      # Vérifie si l'utilisateur a sélectionné au moins une ligne à supprimer.
       if (length(input$table_data_rows_selected) > 0) {
-        newData <- r$data[-input$table_data_rows_selected, ]
-        r$data <- newData
+        # Demande de confirmation avant de procéder à la suppression,
+        confirmSweetAlert(
+          session = session,
+          inputId = "confirmDelete",
+          title = "Confirmation de la suppression",
+          text = "Êtes-vous sûr de vouloir supprimer les lignes sélectionnées ?",
+          type = "warning",
+          danger_mode = TRUE,
+          btn_labels = c("Annuler", "Confirmer")
+        )
+      } else {
+        # Si aucune ligne n'est sélectionnée, informe l'utilisateur via une notification d'erreur.
+        showNotification("Aucune ligne sélectionnée pour la suppression.", type = "error", duration = 4)
       }
-      saveRDS(r$data,"Tonnage.rda")
-      Tonnage <- readRDS("Tonnage.rda")
-      usethis::use_data(Tonnage, overwrite = TRUE)
     })
 
+    # confirmation de suppression.
+    observeEvent(input$confirmDelete, {
+      # Vérifie si l'utilisateur confirme la suppression.
+      if (isTRUE(input$confirmDelete)) {
+        removeModal()
+        if (length(input$table_data_rows_selected) > 0) {
+          newData <- r$data[-input$table_data_rows_selected, ]
+          r$data <- newData
 
+          # Sauvegarde le dataframe mis à jour dans un fichier CSV nommé 'Tonnage.csv'.
+          write.csv(r$data, "Tonnage.csv", row.names = FALSE)
+          # Mise à jour des données dans l'environnement global pour qu'elles soient disponibles partout dans l'application.
+          Tonnage <- read.csv("Tonnage.csv")
+          usethis::use_data(Tonnage, overwrite = TRUE)
+
+          # Informe l'utilisateur de la réussite de la suppression via une notification.
+          showNotification("Les lignes ont été supprimées avec succès.", type = "error", duration = 4)
+        }
+      }
+    })
+
+    # bouton "Éditer".
     observeEvent(input$edit_btn, {
       selected <- input$table_data_rows_selected
+      # Vérifie si exactement une ligne a été sélectionnée pour l'édition.
       if (length(selected) == 1) {
-        selectedData <- r$data[selected, ]
+        selectedData <- r$data[selected, ]  # Extrait la ligne sélectionnée pour l'édition.
+        # Affiche un dialogue modal pour permettre à l'utilisateur de modifier les valeurs de la ligne.
         showModal(modalDialog(
           title = "Éditer la ligne",
           textInput(ns("edit_region"), "Region", selectedData$Region),
@@ -198,18 +335,19 @@ mod_TabData_server <- function(id,r){
           numericInput(ns("edit_tonnages"), "Tonnages", value = selectedData$Tonnages.DIM),
           numericInput(ns("edit_rotations"), "Rotations", value = selectedData$Nbre.de.rotation),
           selectInput(ns("edit_compacteur"), "Compacteur", choices = c("Oui" = 1, "Non" = 0), selected = selectedData$Compacteur),
-          footer = tagList(
+          footer = tagList(  # Boutons pour annuler ou sauvegarder les modifications.
             modalButton("Annuler"),
             actionButton(ns("save_edit"), "Sauvegarder", class = "btn-primary")
           )
         ))
       }
-
     })
 
+    # sauvegarde après l'édition.
     observeEvent(input$save_edit, {
       error_message <- NULL
 
+      # Vérifie les conditions pour assurer que les valeurs éditées sont valides.
       if (as.numeric(input$edit_tonnages) < 0) {
         error_message <- "La valeur du tonnages de DIM doit être positive."
       }
@@ -223,6 +361,7 @@ mod_TabData_server <- function(id,r){
         removeModal()
         selected <- input$table_data_rows_selected
         if (length(selected) == 1) {
+          # Met à jour les données dans l'objet réactif avec les nouvelles valeurs saisies par l'utilisateur.
           r$data[selected, ]$Region <- input$edit_region
           r$data[selected, ]$Maison.Mere <- input$edit_maisonMere
           r$data[selected, ]$Site <- input$edit_site
@@ -231,13 +370,14 @@ mod_TabData_server <- function(id,r){
           r$data[selected, ]$Compacteur <- as.numeric(input$edit_compacteur)
         }
 
-        saveRDS(r$data,"Tonnage.rda")
-        Tonnage <- readRDS("Tonnage.rda")
+        # Sauvegarde de la dataframe mis à jour dans un fichier CSV nommé 'Tonnage.csv'.
+        write.csv(r$data, "Tonnage.csv", row.names = FALSE)
+        # Mise à jour des données dans l'environnement global pour qu'elles soient disponibles partout dans l'application.
+        Tonnage <- read.csv("Tonnage.csv")
         usethis::use_data(Tonnage, overwrite = TRUE)
+
       }
     })
-
-
 
     # Sauvegarde de data au format csv
 
@@ -252,6 +392,4 @@ mod_TabData_server <- function(id,r){
 
   })
 }
-
-
 
--- data-raw/Tonnage.R
+++ data-raw/Tonnage.R
@@ -1,6 +1,6 @@
 ## code to prepare `Tonnage` dataset goes here
 
-Tonnage <- read.csv("https://www.dropbox.com/scl/fi/vd6nhf3e7g14cv9grnif9/tonnage.csv?rlkey=fiw5wpjm2m1ynf9zunydd6cgh&dl=1")
+Tonnage <- read.csv("tonnage.csv")
 usethis::use_data(Tonnage, overwrite = TRUE)
 
 historique <- read.csv("historique.csv")
